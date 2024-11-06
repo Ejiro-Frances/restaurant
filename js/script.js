1fdddcc6dbad118951d14f15c0b0a6a5a26d7d23
Ejiro-Frances <francesejiro@yahoo.com>
2024-11-06 08:29:48 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.querySelector(".hamburger-menu");
-  const menu = document.querySelector(".mobile-menu");
+  const mobileMenu = document.querySelector(".mobile-menu");
   const body = document.body;
+  const searchImage = document.querySelector(".search-bar img");
 
   hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("active");
-    menu.classList.toggle("active");
-    // body.style.overflow = mobileMenu.classList.contains("active")
-    //   ? "hidden"
-    //   : "";
+    mobileMenu.classList.toggle("active");
+    body.style.overflow = mobileMenu.classList.contains("active")
+      ? "hidden"
+      : "";
   });
 
   // Close mobile menu when clicking on a link
-  const mobileLinks = document.querySelectorAll(".mobile-nav-links a");
+  const mobileLinks = document.querySelectorAll(".mobile-menu a");
   mobileLinks.forEach((link) => {
     link.addEventListener("click", function () {
       hamburger.classList.remove("active");
@@ -27,6 +28,18 @@ document.addEventListener("DOMContentLoaded", function () {
       hamburger.classList.remove("active");
       mobileMenu.classList.remove("active");
       body.style.overflow = "";
+    }
+  });
+  // Open the search bar when the search button is clicked
+  searchImage.addEventListener("click", function () {
+    const searchInput = document.querySelector("#search");
+    if (
+      searchInput.style.display === "none" ||
+      searchInput.style.display === ""
+    ) {
+      searchInput.style.display = "block";
+    } else {
+      searchInput.style.display = "none";
     }
   });
 });
